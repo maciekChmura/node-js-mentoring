70a72149281941bb9785d3bdcd392b2cd322f706
Maciek Chmura <chmura.m@gmail.com>
2018-04-17 15:54:35 +0200

    progress on task1

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import config from './config/config';
+import User from './models/User'
+import Product from './models/Product'
+// import { User, Product } from 'models'
+
+console.log(config.name);
+
+const product = new Product;
+const user = new User;
