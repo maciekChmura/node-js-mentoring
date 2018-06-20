72921d1c68e664cc4136b0dc1131bb7111ebc11f
Maciek Chmura <chmura.m@gmail.com>
2018-06-20 21:22:08 +0200

    https implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 'use strict';
 
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 
+const httpsOptions = {
+  key: fs.readFileSync('./csr.pem'),
+  cert: fs.readFileSync('./server.crt'),
+};
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+const httpsPort = process.env.HTTPSPORT || 443;
+http.createServer(app).listen(port);
+https.createServer(httpsOptions, app).listen(httpsPort);
+
