9e02e4e5ece6ee4eb52f3666b6826c248a8d4a7f
Maciek Chmura <chmura.m@gmail.com>
2018-07-04 14:07:30 +0200

    task7a async wrapper to catch errors

--- src/mongoFiles/db.js
+++ src/mongoFiles/db.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const URI = 'mongodb://luke:4qwerty@ds217921.mlab.com:17921/node-mentoring';
 
 function connect(url) {
-  return MongoClient.connect(url).then(client => client.db('node-mentoring'));
+  return MongoClient.connect(url, { useNewUrlParser: true }).then(client => client.db('node-mentoring'));
 }
 
 const db = async () => {
--- src/mongoFiles/routes.js
+++ src/mongoFiles/routes.js
@@ -1,18 +1,14 @@
 'use strict';
 
+const asyncWrapper = require('./asyncWraper');
+
 const getRandomItem = array => array[Math.floor(Math.random() * array.length)];
 
 const routes = (app, dbs) => {
-  app.get('/', (req, res) => {
-    dbs.collection('cities').find({}).toArray((err, docs) => {
-      if (err) {
-        console.log(err);
-        res.error(err);
-      } else {
-        res.json(getRandomItem(docs));
-      }
-    });
-  });
+  app.get('/', asyncWrapper(async (req, res) => {
+    const cursor = await dbs.collection('cities').find({}).toArray();
+    res.json(getRandomItem(cursor));
+  }));
   return app;
 };
 
